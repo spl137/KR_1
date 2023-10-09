8dea386cd9ff9af3b54e6e1f30c1db6e981a4c36
spl137 <137485011+spl137@users.noreply.github.com>
2023-10-09 17:11:59 +0300

    Add files via upload

---
+++ Задание 1.pas
@@ -0,0 +1,29 @@
+﻿var
+  c, d, x, e: real;
+
+begin
+  writeln('Введите X');
+  readln(x);
+  if x < -8 then
+  begin
+    e:= 2.7;
+    c := Cos(x) * -x - power(e, x);
+writeln(c);
+end;  
+  if x < -5 then
+      if x > -9 then
+        begin
+  writeln('Такой функции не существует');
+end;
+  if x < 5 then
+      if x > -6 then
+        begin
+    c := Cos(x) / Cos(x);
+writeln(c);
+end;
+    if x > 4 then
+      begin
+c := sin(x) / cos(x);
+  writeln(c);
+  end;
+end.
---
+++ Задание 2.pas
@@ -0,0 +1,31 @@
+﻿var
+  c, d, x, e: real;
+begin
+  x:=-10;
+ while x<8 do begin
+  if x < -8 then
+  begin
+    e:= 2.7;
+    c := Cos(x) * -x - power(e, x);
+writeln(c);
+end;  
+  if x < -5 then
+      if x > -9 then
+        begin
+  writeln('Такой функции не существует');
+end;
+  if x < 5 then
+      if x > -6 then
+        begin
+    c := Cos(x) / Cos(x);
+writeln(c);
+end;
+    if x > 4 then
+      begin
+c := sin(x) / cos(x);
+  writeln(c);
+  
+  end;
+  x:=x+0.1;
+  end;
+end.
